264887e20c53a24e5916fce6ce2f3b59cb08fee6
Brian Omondi <brianhilsden@gmail.com>
2024-04-30 09:46:49 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ const { useEffect, useState } = require("react")
 
 // create your App component here
 function App(){
-    const [loading,setLoading] = useState(false)
+  
     const [image,setImage] = useState("")
     useEffect(()=>{
         fetch("https://dog.ceo/api/breeds/image/random")
         .then(res=>res.json())
         .then(data=>setImage(data.message));
-        setLoading(true)
-    })
+      
+    },[])
 
     return(
         <div>
